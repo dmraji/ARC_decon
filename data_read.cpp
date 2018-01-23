9e8a2bcdf51bb67520e520bcc7e0d491f46d492a
dmraji <davidmraji@gmail.com>
2018-01-23 17:36:20 -0500

    Working test data read

---
+++ data_read.cpp
@@ -0,0 +1,37 @@
+// Reading in radiation detector output files
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+  ifstream in_file;
+
+  in_file.open("test.txt");
+
+  // Check that file actually exists
+  if (!in_file) {
+    cout << "File open failed.\n";
+    exit(1);
+  }
+
+  std::vector<int>counts_by_ch;
+  int num;
+
+  while(in_file >> num) {
+    counts_by_ch.push_back(num);
+  }
+
+  for (std::vector<int>::const_iterator i = counts_by_ch.begin(); i != counts_by_ch.end(); ++i) {
+    std::cout << *i << ' ';
+  }
+
+  in_file.close();
+
+  cout << "done\n";
+  return 0;
+}
